330bb04608e641c5f8949adf81820ccddea63f46
Crowdbotics <engineering@crowdbotics.com>
2023-01-28 00:08:37 +0000

    Initial commit.

---
+++ backend/testautoapp159_dev_59597/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testautoapp159_dev_59597 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testautoapp159_dev_59597.settings')
+
+application = get_wsgi_application()
